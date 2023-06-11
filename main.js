3d8f0a3d685f982eca3ad0667a3987087a3d60a5
MD.Abid Hasan <124032625+Abid0170@users.noreply.github.com>
2023-06-11 00:20:26 -0500

    html javascript

---
+++ main.js
@@ -0,0 +1,8 @@
+//DOM selector (multiple elements)
+/*
+let list =document.getElementsByClassName('simple-class');
+list[0].style.background = 'red';
+list[0].style.color = '#ffffff'
+console.log(list[0]);*/
+let list = document.getElementsByTagName('li');
+console.log(list);
